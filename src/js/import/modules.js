fac8d33c3931b89dd605c6a6a16e95ee5715945c
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2021-11-22 23:19:05 +0200

    section info complete

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -2,3 +2,4 @@ import "%modules%/header/open-form";
 import "%modules%/header/burger";
 import "%modules%/hero/hero-slider";
 import "%modules%/logos/logos-slider";
+import "%modules%/collection-section/collection-section-slider";
