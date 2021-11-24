7881147020f4d6ab0e4104dd508ac5ac73356a04
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2021-11-24 20:29:49 +0200

    some fix

--- src/blocks/modules/logos/logos-slider.js
+++ src/blocks/modules/logos/logos-slider.js
@@ -1,13 +1,33 @@
-import Swiper, { FreeMode } from 'swiper';
+import Swiper from 'swiper';
+import ScrollTrigger from "gsap/ScrollTrigger";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const showDemo = () => {
+
+    gsap.utils.toArray('.logos').forEach((section, index) => {
+        const w = section.querySelector('.logos-slider .swiper-wrapper');
+        const [x, xEnd] = (index % 2) ? ['100%', (w.scrollWidth - section.offsetWidth) * -1] : [w.scrollWidth * -1, 0];
+        gsap.fromTo(w, {
+            x
+        }, {
+            x: xEnd,
+            scrollTrigger: {
+                trigger: section,
+                scrub: 0.5
+            }
+        });
+    });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     new Swiper('.logos-slider', {
-        modules: [FreeMode],
-        freeMode: true,
-        grabCursor: true,
         slidesPerView: 'auto',
         spaceBetween: 22,
         loop: true,
         centeredSlides: true,
     });
 });
+
+showDemo()
