5423ae70205c6cf22a5f62558b12598a157dfef3
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2021-11-24 17:32:25 +0200

    some fix

--- src/blocks/modules/hero/hero-slider.js
+++ src/blocks/modules/hero/hero-slider.js
@@ -1,11 +1,20 @@
-import Swiper, { Navigation } from 'swiper';
+import Swiper, {
+    Navigation,
+    Pagination
+} from 'swiper';
 
 document.addEventListener("DOMContentLoaded", () => {
     new Swiper('.hero-slider', {
-        modules: [Navigation],
+        modules: [Navigation, Pagination],
         navigation: {
             nextEl: '.hero-slider__btn--next',
             prevEl: '.hero-slider__btn--prev',
-          },
+        },
+
+        pagination: {
+            el: '.hero-slider__pag',
+            type: 'bullets',
+            clickable: true,
+        },
     });
 });
