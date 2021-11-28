050438485e0a83225fa0bad3198898451707ecba
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2021-11-28 21:09:01 +0200

    some fix

--- src/blocks/components/filters.js
+++ src/blocks/components/filters.js
@@ -22,7 +22,7 @@ export const filters = () => {
                     let el = filtersContent[i];
                     el.classList.remove('g-filters__content--active')
                 }
-                element.closest(".g-filters").querySelector(`[data-filters-target="${path}"]`).classList.add('g-filters__content--active');
+                element.closest(".g-filters")?.querySelector(`[data-filters-target="${path}"]`)?.classList.add('g-filters__content--active');
             };
 
 
