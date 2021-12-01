5aa1eb93560b09181277da1ffcda23e71970193d
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2021-12-01 21:38:08 +0200

    block region selection complete

---
+++ src/blocks/modules/header/toggle-location.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    /* Variables */
+
+    const toggleLocationBtns = document.querySelectorAll(".toggle-location");
+    const regionSelection = document.querySelector(".region-selection");
+    const regionSelectionBtns = document.querySelectorAll(".js-region-selection__btn");
+    const regionSelectionChoice = document.querySelectorAll(".region-selection__choice");
+    const regionSelectionClose = document.querySelector(".region-selection__close");
+    const body = document.body;
+
+    /* Functions */
+
+    function toggleLocation() {
+        regionSelection.classList.toggle("isOpen");
+        body.classList.toggle("lock")
+    }
+
+    function hiddenLocation() {
+        regionSelection.classList.remove("isOpen");
+        body.classList.remove("lock")
+    }
+
+    function changeLocation(e) {
+        regionSelectionChoice.forEach(el => el.textContent = e.currentTarget.textContent);
+        hiddenLocation();
+    }
+
+    
+    /* Events */
+
+    toggleLocationBtns.forEach(btn => {
+        btn.addEventListener("click", toggleLocation);
+    });
+
+    regionSelectionBtns.forEach(btn => {
+        btn.addEventListener("click", changeLocation);
+    });
+
+    regionSelectionClose.addEventListener("click", hiddenLocation);
+
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') hiddenLocation();
+    });
+});
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,4 +1,5 @@
 import "%modules%/header/open-form";
+import "%modules%/header/toggle-location";
 import "%modules%/header/burger";
 import "%modules%/hero/hero-slider";
 import "%modules%/logos/logos-slider";
