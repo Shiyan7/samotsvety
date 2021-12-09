7b14b4b107adb5e13574816852002709e840e223
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2021-12-09 23:58:39 +0200

    some fix

--- src/js/components/popular-categories-more.js
+++ src/js/components/popular-categories-more.js
@@ -2,10 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const moreCategories = document.querySelector(".more-categories");
     const popularCategories = document.querySelector(".popular-categories");
     const hiddenCategories = document.querySelectorAll(".popular-category--hidden");
+    const popularCategoriesItems = document.querySelectorAll(".popular-category__item");
 
     moreCategories?.addEventListener("click", (e) => {
+        
         popularCategories.classList.add("active");
         hiddenCategories.forEach(el => el.classList.remove("popular-category--hidden"));
-        hiddenCategories.forEach(el => el.classList.add("popular-category--active"));
+
+        popularCategoriesItems.forEach(el => {
+            TweenMax.staggerTo(el, .5, {
+                scale: 1,
+                opacity: 1,
+                ease: Back.easeIn.config(1)
+            }, .3);
+        })
     });
 });
