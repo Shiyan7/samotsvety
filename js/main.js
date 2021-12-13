766d8eac5ad58981f219f3f11cf1e54800f8b438
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2021-12-14 00:39:07 +0200

    some fix

--- js/main.js
+++ js/main.js
@@ -390,6 +390,54 @@ var observer = lozad(".lozad");
 observer.observe();
 "use strict";
 
+var center = [48.8866527839977, 2.34310679732974];
+
+function init() {
+  var map = new ymaps.Map('map', {
+    center: center,
+    zoom: 17
+  });
+  var placemark = new ymaps.Placemark(center, {
+    balloonContentHeader: 'Хедер балуна',
+    balloonContentBody: 'Боди балуна',
+    balloonContentFooter: 'Подвал'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: 'https://image.flaticon.com/icons/png/512/64/64113.png',
+    iconImageSize: [40, 40],
+    iconImageOffset: [-19, -44]
+  });
+  var placemark1 = new ymaps.Placemark(center, {
+    balloonContent: "\n\n\t\t\t<div class=\"balloon\">\n\t\t\t</div>\n\n\t\t"
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: 'https://image.flaticon.com/icons/png/512/64/64113.png',
+    iconImageSize: [40, 40],
+    iconImageOffset: [-19, -44]
+  });
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+
+  map.controls.remove('searchControl'); // удаляем поиск
+
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+
+  map.controls.remove('typeSelector'); // удаляем тип
+
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+  // map.geoObjects.add(placemark);
+
+  map.geoObjects.add(placemark1);
+  placemark1.balloon.open();
+}
+
+ymaps.ready(init);
+"use strict";
+
 document.addEventListener("DOMContentLoaded", function () {
   var menuSliders = document.querySelectorAll(".menu-slider");
   menuSliders.forEach(function (slider) {
