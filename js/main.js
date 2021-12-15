8c711b5e30e2e56f2ec83b996172d7ecfa1f3d0f
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2021-12-15 15:51:26 +0200

    some fix

--- js/main.js
+++ js/main.js
@@ -395,72 +395,47 @@ document.addEventListener("DOMContentLoaded", function () {
   maps.forEach(function (el) {
     var _window$ymaps;
 
-    var center = [48.8866527839977, 2.34310679732974];
-
     function init() {
       var mapCenter = [48.8866527839977, 2.34310679732974],
           map = new ymaps.Map(el, {
         center: mapCenter,
         zoom: 10,
         controls: []
-      });
-      /* placemarks = []; */
+      }); // Создаем собственный макет с информацией о выбранном геообъекте.
 
-      /* // Создаем собственный макет с информацией о выбранном геообъекте.
-      var customBalloonContentLayout = ymaps.templateLayoutFactory.createClass([
-      	`
-       
-      	<div class="offices-item map-item">
-      		<img class="offices-item__image map-item__image" src="img/no-image.svg" alt="Офис 1">
-      		<div class="store-item__right map-item__right">
-      			<a class="offices-item__desc offices-item__single-tel" href="#">Тел.: 8 (800) 555-32-98</a>
-      			<p class="offices-item__desc offices-item__location">г. Санкт-Петербург, пл. Фаберже, 8</p>
-      			<span class="offices-item__metro offices-item__metro--blue">
-      			<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-      				<path d="M7.74363 8.24606C8.49925 7.42446 9.40929 6.75974 10.4218 6.28984C11.4343 5.81995 12.5295 5.55408 13.6447 5.50743C14.76 5.46077 15.8735 5.63424 16.9217 6.01793C17.97 6.40162 18.9323 6.98801 19.7539 7.74363C20.5755 8.49925 21.2403 9.40929 21.7102 10.4218C22.1801 11.4343 22.4459 12.5295 22.4926 13.6447C22.5392 14.76 22.3658 15.8735 21.9821 16.9217C21.5984 17.97 21.012 18.9323 20.2564 19.7539C19.5008 20.5755 18.5907 21.2403 17.5782 21.7102C16.5657 22.1801 15.4705 22.4459 14.3553 22.4926C13.24 22.5392 12.1265 22.3658 11.0783 21.9821C10.03 21.5984 9.06766 21.012 8.24606 20.2564C7.42446 19.5008 6.75974 18.5907 6.28984 17.5782C5.81995 16.5657 5.55408 15.4705 5.50743 14.3553C5.46077 13.24 5.63424 12.1265 6.01793 11.0783C6.40162 10.03 6.98801 9.06766 7.74363 8.24606L7.74363 8.24606Z" stroke="var(--color)"></path>
-      				<path d="M18 10V18H16.751V13.045L14.3939 18H13.6061L11.2426 13.045V18H10V10H11.044L13.9968 16.1581L16.9496 10H18Z" fill="var(--color)"></path>
-      			</svg>
-      			<span class="offices-item__desc">ст. м. Технологический институт - 2</span>
-      			</span>
-      			<p class="offices-item__desc offices-item__hours">Пн—Пт с 10:00 до 21:00 Сб—Вс с 10:00 до 17:00</p><a
-      			class="offices-item__link" href="mailto:jewel@russammarket.ru">jewel@russammarket.ru</a>
-      		</div>
-      	</div>
-      
-      	`
-      ].join(''));
-      		var clusterer = new ymaps.Clusterer({
-      	clusterDisableClickZoom: true,
-      	clusterOpenBalloonOnClick: true,
-      	// Устанавливаем режим открытия балуна. 
-      	// В данном примере балун никогда не будет открываться в режиме панели.
-      	clusterBalloonPanelMaxMapArea: 0,
-      	// По умолчанию опции балуна balloonMaxWidth и balloonMaxHeight не установлены для кластеризатора,
-      	// так как все стандартные макеты имеют определенные размеры.
-      	clusterBalloonMaxHeight: 200,
-      	// Устанавливаем собственный макет контента балуна.
-      	clusterBalloonContentLayout: customBalloonContentLayout
+      var customBalloonContentLayout = ymaps.templateLayoutFactory.createClass(["\n    \n\t\t\t\t<div class=\"offices-item map-item\">\n\t\t\t\t\t<img class=\"offices-item__image map-item__image\" src=\"img/no-image.svg\" alt=\"\u041E\u0444\u0438\u0441 1\">\n\t\t\t\t\t<div class=\"store-item__right map-item__right\">\n\t\t\t\t\t\t<a class=\"offices-item__desc offices-item__single-tel\" href=\"#\">\u0422\u0435\u043B.: 8 (800) 555-32-98</a>\n\t\t\t\t\t\t<p class=\"offices-item__desc offices-item__location\">\u0433. \u0421\u0430\u043D\u043A\u0442-\u041F\u0435\u0442\u0435\u0440\u0431\u0443\u0440\u0433, \u043F\u043B. \u0424\u0430\u0431\u0435\u0440\u0436\u0435, 8</p>\n\t\t\t\t\t\t<span class=\"offices-item__metro offices-item__metro--blue\">\n\t\t\t\t\t\t\t<svg width=\"28\" height=\"28\" viewBox=\"0 0 28 28\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n\t\t\t\t\t\t\t\t<path d=\"M7.74363 8.24606C8.49925 7.42446 9.40929 6.75974 10.4218 6.28984C11.4343 5.81995 12.5295 5.55408 13.6447 5.50743C14.76 5.46077 15.8735 5.63424 16.9217 6.01793C17.97 6.40162 18.9323 6.98801 19.7539 7.74363C20.5755 8.49925 21.2403 9.40929 21.7102 10.4218C22.1801 11.4343 22.4459 12.5295 22.4926 13.6447C22.5392 14.76 22.3658 15.8735 21.9821 16.9217C21.5984 17.97 21.012 18.9323 20.2564 19.7539C19.5008 20.5755 18.5907 21.2403 17.5782 21.7102C16.5657 22.1801 15.4705 22.4459 14.3553 22.4926C13.24 22.5392 12.1265 22.3658 11.0783 21.9821C10.03 21.5984 9.06766 21.012 8.24606 20.2564C7.42446 19.5008 6.75974 18.5907 6.28984 17.5782C5.81995 16.5657 5.55408 15.4705 5.50743 14.3553C5.46077 13.24 5.63424 12.1265 6.01793 11.0783C6.40162 10.03 6.98801 9.06766 7.74363 8.24606L7.74363 8.24606Z\" stroke=\"var(--color)\"></path>\n\t\t\t\t\t\t\t\t<path d=\"M18 10V18H16.751V13.045L14.3939 18H13.6061L11.2426 13.045V18H10V10H11.044L13.9968 16.1581L16.9496 10H18Z\" fill=\"var(--color)\"></path>\n\t\t\t\t\t\t\t</svg>\n\t\t\t\t\t\t\t<span class=\"offices-item__desc\">\u0441\u0442. \u043C. \u0422\u0435\u0445\u043D\u043E\u043B\u043E\u0433\u0438\u0447\u0435\u0441\u043A\u0438\u0439 \u0438\u043D\u0441\u0442\u0438\u0442\u0443\u0442 - 2</span>\n\t\t\t\t\t\t</span>\n\t\t\t\t\t\t<p class=\"offices-item__desc offices-item__hours\">\u041F\u043D\u2014\u041F\u0442 \u0441 10:00 \u0434\u043E 21:00  \u0421\u0431\u2014\u0412\u0441 \u0441 10:00 \u0434\u043E 17:00</p>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t\n\t\t\t\t"].join(''));
+      var clusterer = new ymaps.Clusterer({
+        clusterDisableClickZoom: true,
+        clusterOpenBalloonOnClick: true,
+        // Устанавливаем режим открытия балуна. 
+        // В данном примере балун никогда не будет открываться в режиме панели.
+        clusterBalloonPanelMaxMapArea: 0,
+        // По умолчанию опции балуна balloonMaxWidth и balloonMaxHeight не установлены для кластеризатора,
+        // так как все стандартные макеты имеют определенные размеры.
+        clusterBalloonMaxHeight: 136,
+        // Устанавливаем собственный макет контента балуна.
+        clusterBalloonContentLayout: customBalloonContentLayout
       });
-      		// Заполняем кластер геообъектами со случайными позициями.
+      var placemarks = []; // Заполняем кластер геообъектами со случайными позициями.
+
       for (var i = 0, l = 100; i < l; i++) {
-      	var placemark = new ymaps.Placemark(center, {
-      		// Устаналиваем данные, которые будут отображаться в балуне.
-      		balloonContentHeader: 'Заголовок метки №' + (i + 1),
-      		balloonContentBody: 'Информация о метке №' + (i + 1),
-      		placemarkId: i
-      	});
-      	placemarks.push(placemark);
+        var placemark = new ymaps.Placemark(mapCenter, {
+          // Устаналиваем данные, которые будут отображаться в балуне.
+          balloonContentHeader: 'Заголовок метки №' + (i + 1),
+          balloonContentBody: 'Информация о метке №' + (i + 1),
+          placemarkId: i
+        });
+        placemarks.push(placemark);
       }
-      		clusterer.add(placemarks);
+
+      clusterer.add(placemarks);
       map.geoObjects.add(clusterer);
-      
+
       function getRandomPosition() {
-      	return [
-      		mapCenter[0] + (Math.random() * 0.3 - 0.15),
-      		mapCenter[1] + (Math.random() * 0.5 - 0.25)
-      	];
+        return [mapCenter[0] + (Math.random() * 0.3 - 0.15), mapCenter[1] + (Math.random() * 0.5 - 0.25)];
       }
-      clusterer.balloon.open(clusterer.getClusters()[0]); */
+
+      clusterer.balloon.open(clusterer.getClusters()[0]);
     }
 
     (_window$ymaps = window.ymaps) === null || _window$ymaps === void 0 ? void 0 : _window$ymaps.ready(init);
