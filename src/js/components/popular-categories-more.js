e04a1c0cb28c3d4cda790e60eb1d73b7d414a3e1
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2021-12-17 21:56:21 +0200

    some fix

--- src/js/components/popular-categories-more.js
+++ src/js/components/popular-categories-more.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const moreCategories = document.querySelector(".more-categories");
-    const popularCategories = document.querySelector(".popular-categories");
     const hiddenCategories = document.querySelectorAll(".popular-category--hidden");
     const popularCategoriesItems = document.querySelectorAll(".popular-category__item");
 
     moreCategories?.addEventListener("click", (e) => {
         
-        popularCategories.classList.add("active");
         hiddenCategories.forEach(el => el.classList.remove("popular-category--hidden"));
 
         popularCategoriesItems.forEach(el => {
