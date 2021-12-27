61b3ea6f239310f2202424f57d8a06b75efaac9a
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2021-12-27 16:33:34 +0200

    some fix

--- src/js/components/inputmask-settings.js
+++ src/js/components/inputmask-settings.js
@@ -1,7 +1,3 @@
-/* const tels = document.querySelectorAll("input[type=\"tel\"]");
-const im = new Inputmask("+7 (999) 999-99-99");
-im.mask(tels); */
-
 const tels = document.querySelectorAll("input[type=\"tel\"]");
 
 tels.forEach(el => {
--- src/js/components/select.js
+++ src/js/components/select.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if(selects) {
         selects.forEach(el => {
             const choices = new Choices(el, {
+                searchPlaceholderValue: el.dataset.placeholder,
                 itemSelectText: '',
                 shouldSort: false,
                 noResultsText: 'Не найдено'
