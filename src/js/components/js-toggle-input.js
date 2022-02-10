0a93dfc9e9c8552497c04987a41b3bda1a50b0bc
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-02-10 19:19:10 +0200

    отрефакторил жс

--- src/js/components/js-toggle-input.js
+++ src/js/components/js-toggle-input.js
@@ -1,28 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const cabinetContentInputs = document.querySelectorAll(".js-toggle-input");
-
-    let flag = 0;
-
-    function clearValue (inputs) {
-        inputs.value = null
-    }
     
     function toggleInputs () {
         let txt = this.querySelector("span");
         let target = this.dataset.text;
         let parent = this.closest(".cabinet-accordion__content");
-        let passwordInputs = parent.querySelectorAll(".g-input-password__input")
         parent.classList.toggle("active");
         this.classList.add("cabinet-content__edit--active");
-
-        if(flag == 0) {
-            txt.innerHTML = 'Сохранить изменения'
-            flag++
-        } else {
-            txt.innerHTML = target
-            /* passwordInputs.forEach(el => clearValue(el)) */
-            flag--
-        }
+        txt.innerHTML !== 'Сохранить изменения' ? txt.innerHTML = 'Сохранить изменения' : txt.innerHTML = target;
     }
 
     cabinetContentInputs.forEach(el => el.addEventListener("click", toggleInputs))
