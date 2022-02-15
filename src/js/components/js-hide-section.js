d5356a04851bd3edb839bdba06b7526d41d7aeca
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-02-15 15:43:51 +0200

    fix hide section

--- src/js/components/js-hide-section.js
+++ src/js/components/js-hide-section.js
@@ -2,8 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const jsHideBtns = document.querySelectorAll(".js-hide-btn-section");
 
     function hideSection () {
-        let section = this.closest(".js-section");
-        section.remove();
+        let content = this.closest(".js-section").querySelector(".js-section-content")
+
+        this.innerText !== 'Показать блок:' ? this.innerText = 'Показать блок:' : this.innerText = 'Скрыть блок:';
+
+        this.classList.toggle("active")
+        content.classList.toggle("hide-section")
     }
 
     jsHideBtns?.forEach(el => el.addEventListener("click", hideSection));
