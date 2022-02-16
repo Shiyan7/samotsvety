cfdba7fa7ba988549f366a6189547d1c64fec778
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-02-16 18:11:47 +0200

    some fix

--- src/js/components/js-toggle-input.js
+++ src/js/components/js-toggle-input.js
@@ -5,9 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
         let txt = this.querySelector("span");
         let target = this.dataset.text;
         let parent = this.closest(".cabinet-accordion__content");
-        parent.classList.toggle("active");
-        this.classList.add("cabinet-content__edit--active");
-        txt.innerHTML !== 'Сохранить изменения' ? txt.innerHTML = 'Сохранить изменения' : txt.innerHTML = target;
+        if(!parent.querySelector("input.error")) {
+            parent.classList.toggle("active");
+            this.classList.add("cabinet-content__edit--active");
+            txt.innerHTML !== 'Сохранить изменения' ? txt.innerHTML = 'Сохранить изменения' : txt.innerHTML = target;
+        }
     }
 
     cabinetContentInputs?.forEach(el => el.addEventListener("click", toggleInputs))
