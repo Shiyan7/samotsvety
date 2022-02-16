48205816424b68c50a8fe0c1d39489458320a3d0
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-02-16 20:10:55 +0200

    added check for length

--- src/js/components/js-toggle-input.js
+++ src/js/components/js-toggle-input.js
@@ -5,7 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
         let txt = this.querySelector("span");
         let target = this.dataset.text;
         let parent = this.closest(".cabinet-accordion__content");
-        if(!parent.querySelector("input.error")) {
+        let input = parent.querySelector("input");
+        if(!input.classList.contains("error") && input.value.length > 0) {
             parent.classList.toggle("active");
             this.classList.add("cabinet-content__edit--active");
             txt.innerHTML !== 'Сохранить изменения' ? txt.innerHTML = 'Сохранить изменения' : txt.innerHTML = target;
