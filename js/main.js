b067ce5ce5b369833c343ff7eb2d727b397fe1b5
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2022-02-16 20:11:06 +0200

    added check for length

--- js/main.js
+++ js/main.js
@@ -494,8 +494,9 @@ document.addEventListener("DOMContentLoaded", function () {
     var txt = this.querySelector("span");
     var target = this.dataset.text;
     var parent = this.closest(".cabinet-accordion__content");
+    var input = parent.querySelector("input");
 
-    if (!parent.querySelector("input.error")) {
+    if (!input.classList.contains("error") && input.value.length > 0) {
       parent.classList.toggle("active");
       this.classList.add("cabinet-content__edit--active");
       txt.innerHTML !== 'Сохранить изменения' ? txt.innerHTML = 'Сохранить изменения' : txt.innerHTML = target;
